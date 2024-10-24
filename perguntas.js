5751c673fe22fd0b802e882f774c7a7e8c3c2926
ckantonio8 <143199336+ckantonio8@users.noreply.github.com>
2024-10-24 19:49:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Mitologia Grega',
+    'Qual é o simbolismo do mito de Prometeu no contexto da relação entre os deuses e a humanidade?',
+    'Prometeu simboliza a rebeldia contra o poder divino e o progresso humano, já que ao dar o fogo à humanidade, ele desafiou Zeus e promoveu o desenvolvimento da civilização.'
+)
+
+criaCartao(
+    'Mitologia Grega',
+    'No começo, qual foi o deus criador do universo na cosmogonia grega?',
+    'Caos (em grego: Χάος, transl.: Cháos), na mitologia grega segundo Hesíodo, é o primeiro deus primordial a surgir no universo, portanto a mais velha das formas de consciência divina.'
+)
+
+criaCartao(
+    'Mitologia Grega',
+    'Qual a importância do mito de Édipo na tragédia e na psicanálise moderna?',
+    
+    'O mito de Édipo é central na tragédia grega, abordando temas de destino e culpa, e influenciou a psicanálise, com o "complexo de Édipo", que explora a relação psicológica entre pais e filhos.'
+)
+criaCartao(
+    'Mitologia Grega',
+    'O que o mito de Perséfone explica sobre as estações do ano na Grécia antiga?',
+    'O mito de Perséfone explica o ciclo das estações: quando Perséfone está no submundo, é inverno; quando retorna à superfície, é primavera, simbolizando o renascimento da natureza.'
+)
+
+criaCartao(
+    'Mitologia Grega',
+    'Como o mito de Odisseu (Ulisses) reflete os valores gregos de astúcia e perseverança?',
+    'Odisseu é celebrado por sua inteligência e resiliência ao superar desafios, refletindo o ideal grego de sabedoria prática e determinação diante das adversidades.'
+)
+
+criaCartao(
+    'Mitologia Grega',
+    'Como o mito de Hércules (Herácles) representa a luta entre virtude e sofrimento?',
+    'Os doze trabalhos de Hércules simbolizam a superação de desafios extremos através de coragem e força, destacando a virtude da perseverança mesmo diante de grande sofrimento.'
+)
+
+criaCartao(
+    'Mitologia Grega',
+    'De que maneira o mito de Narciso explora temas de identidade e vaidade?',
+    'Narciso, que se apaixonou pela própria imagem, representa o perigo do egoísmo e da vaidade extrema, sendo um mito que adverte contra a auto-obsessão e a desconexão com os outros.'
+)
